867f0fd2de26ada6ace034e1b619e148b7315c0d
surekha25 <surekhasuri25@gmail.com>
2025-10-21 16:19:17 +0530

    conditions and count (ec2 and r53 records usins count) folders added

---
+++ conditions/ec2.tf
@@ -0,0 +1,32 @@
+resource "aws_instance" "terraform" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = var.environment == "dev" ? "t3.micro" : "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_all.id]
+  tags = {
+    Name = "terraform"
+    Terraform = "True"
+  }
+}
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all_for_terraform"
+  description = "Allow all for terraform project"
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]    
+  }
+
+  ingress {
+    from_port        = 0 # from port 0 - to port 0 means all ports
+    to_port          = 0
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  tags = {
+    Name = "allow-all-terraform"
+  }
+}
