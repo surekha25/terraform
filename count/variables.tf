bb3ca1a8fdd42b952c090c16a9c0da5ee6d0e6ff
surekha25 <surekhasuri25@gmail.com>
2025-10-21 15:53:57 +0530

    conditions and count (ec2 and r53 records usins count) folders added

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = [ "mangodb", "catalogue", "mysql", "redis" ]
+}
+
+variable "zone_id"{
+    default = "Z03322182ZV3F5YEDZYG5"
+}
+
+variable "domain_name"{
+    default = "surekhadevops.biz"
+}
