8fc9d8515a9534bd3a38449cb4d95e2eb49d9394
surekha25 <surekhasuri25@gmail.com>
2025-10-23 14:51:53 +0530

    remote state folder added

---
+++ remote-state/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-remote-state-86s"
+    key    = "remote-state-demo"
+    region = "us-east-1"
+    use_lockfile = true
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
